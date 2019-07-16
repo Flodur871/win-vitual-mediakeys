776de0c50d68c33bcde62b4dfc0f90f1242151e6
Flodur871 <42621369+Flodur871@users.noreply.github.com>
2019-07-16 20:47:59 +0300

    Update cntrl.py

--- cntrl.py
+++ cntrl.py
@@ -4,7 +4,6 @@ from pynput import keyboard
 
 class Ls(object):
     def __init__(self):
-        # keys = {getattr(win32con, v) : v for v in dir(win32con) if v.startswith("VK_")}
         self.kpress = {keyboard.Key.space: 0xb3, keyboard.Key.left: 0xb1, keyboard.Key.up: 0xaf,
                      keyboard.Key.right: 0xb0, keyboard.Key.down: 0xae}
         self.kpress = {x.value.vk: self.kpress[x] for x in self.kpress.keys()}
@@ -26,7 +25,6 @@ class Ls(object):
 
     def fil(self, msg, data):
         if self.ctrl and data.vkCode in self.kpress.keys() and data.flags < 2:
-            print(self.get_methods(data))
             self.key_event(self.kpress[data.vkCode])
             self.listener.suppress_event()
 
@@ -37,11 +35,6 @@ class Ls(object):
     @staticmethod
     def is_control(k):
         return k == keyboard.Key.ctrl_l or k == keyboard.Key.ctrl_r
-
-    @staticmethod
-    def get_methods(obj):
-        return {x: getattr(obj, x) for x in dir(obj) if not x.startswith('_')}
-
 
 if __name__ == '__main__':
     Ls()
