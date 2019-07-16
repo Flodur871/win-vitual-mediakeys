23f3fdf28cf76e9f47874c7738ad765b2d2a5826
Flodur871 <42621369+Flodur871@users.noreply.github.com>
2019-07-16 20:58:02 +0300

    Add files via upload

--- cntrl.py
+++ cntrl.py
@@ -10,6 +10,7 @@ class Ls(object):
         self.kpress[ord('M')] = 0xAD
 
         self.ctrl = False
+        self.enabled = True
         self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release,
                                           win32_event_filter=self.fil)
         self.listener.start()
@@ -18,13 +19,16 @@ class Ls(object):
     def on_press(self, key):
         if self.is_control(key):
             self.ctrl = True
+        
+        if self.ctrl and self.is_shift(key):
+            self.enabled = not self.enabled
 
     def on_release(self, key):
         if self.is_control(key):
             self.ctrl = False
 
     def fil(self, msg, data):
-        if self.ctrl and data.vkCode in self.kpress.keys() and data.flags < 2:
+        if self.enabled and self.ctrl and data.vkCode in self.kpress.keys() and data.flags < 2:
             self.key_event(self.kpress[data.vkCode])
             self.listener.suppress_event()
 
@@ -35,6 +39,11 @@ class Ls(object):
     @staticmethod
     def is_control(k):
         return k == keyboard.Key.ctrl_l or k == keyboard.Key.ctrl_r
+
+    @staticmethod
+    def is_shift(k):
+        return k == keyboard.Key.shift_l or k == keyboard.Key.shift_r
+
 
 if __name__ == '__main__':
     Ls()
