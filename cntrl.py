0dee1fc066de75b8dd8b58adc175d00c5631cda7
Flodur871 <42621369+Flodur871@users.noreply.github.com>
2021-07-04 15:40:45 +0000

    Update cntrl.py

--- cntrl.py
+++ cntrl.py
@@ -1,7 +1,6 @@
 from win32api import keybd_event, GetKeyState
-import pythoncom
 from pynput import keyboard
-from pyWinCoreAudio import AudioDevices
+
 
 class Ls:
     def __init__(self):
@@ -9,20 +8,11 @@ class Ls:
                        keyboard.Key.right: 0xb0, keyboard.Key.down: 0xae}   # keys to media keys
         self.kpress = {x.value.vk: self.kpress[x] for x in self.kpress.keys()}  # extract the values of the special keys
         self.kpress[ord('M')] = 0xAD
-
-        audio_devices = [ad for ad in AudioDevices]     # get all audio devices available 
-        for dev in audio_devices:
-            if dev.id == u'{0.0.0.00000000}.{8f75c60b-93f2-4836-bafb-16779e08260b}':    # my speakers uuid
-                speakers = dev.render_endpoints[0]
-            
-            elif dev.id == u'{0.0.0.00000000}.{bf274eca-18cb-4b20-9c0c-751a1734f755}':  # my headphones uuid
-                headphones = dev.render_endpoints[0]
         
         self.toggler = {keyboard.Key.up: headphones, keyboard.Key.down: speakers}
         self.toggler = {x.value.vk: self.toggler[x] for x in self.toggler.keys()}
 
         self.ctrl = False
-        self.shift = False
         self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release,
                                           win32_event_filter=self.fil)
         self.listener.start()
@@ -35,9 +25,6 @@ class Ls:
         """
         if self.is_control(key):
             self.ctrl = True
-        
-        elif self.is_shift(key):
-            self.shift = True
         
         elif 'value' in dir(key):
             if key.value.vk not in list(self.kpress.values()): # disable overide if hotkey isn't pressed
@@ -63,14 +50,7 @@ class Ls:
         """
         # print(data.flags)
         if not self.is_caps_on() and self.ctrl and data.vkCode in self.kpress.keys() and data.flags < 2:
-            if self.shift:
-                if data.vkCode in list(self.toggler):
-                    pythoncom.CoInitialize()
-                    self.toggler[data.vkCode].set_default()
-
-            else:
-                self.key_event(self.kpress[data.vkCode])    # simulate media key press
-
+            self.key_event(self.kpress[data.vkCode])    # simulate media key press
             self.listener.suppress_event()  # stop the keys from getting sent to the rest of the system
     
 
@@ -97,14 +77,7 @@ class Ls:
         :param k: key pressed
         """
         return k == keyboard.Key.ctrl_l or k == keyboard.Key.ctrl_r
-    
-    @staticmethod
-    def is_shift(k):
-        """
-        return true if shift was pressed
-        :param k: key pressed
-        """
-        return k == keyboard.Key.shift_l or k == keyboard.Key.shift_r
+
 
 
 if __name__ == '__main__':
